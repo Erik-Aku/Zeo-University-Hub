14860b12e3cd9cc08002c5987aa9ce8d883837de
Erik  Enoka Aku <Erik@Jennifers-iMac.lan>
2024-03-17 15:38:33 -0700

    navbar routing, login and signup form pages

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,63 +1,49 @@
-
-// import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
 // import { Navbar, Nav, Container} from 'react-bootstrap';
 
-
 const AppNavbar = () => {
+  return (
+    <>
+      <header>
+        <img
+          id="profilepic"
+          src="./Assets/images/profile pic.png"
+          alt="profilePicture"
+          width="10%"
+          height="10%"
+        />
 
-
-    return (
-    //   <>
-    //     <Navbar bg='dark' variant='dark' expand='lg'>
-    //       <Container fluid>
-    //         <Navbar.Brand as={Link} to='/'>
-    //           University Search
-    //         </Navbar.Brand>
-    //         <Navbar.Toggle aria-controls='navbar' />
-    //         <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
-    //           <Nav className='ml-auto d-flex'>
-    //             <Nav.Link as={Link} to='/'>
-    //               Search For Colleges
-    //             </Nav.Link>
-    //           </Nav>
-    //         </Navbar.Collapse>
-    //       </Container>
-    //     </Navbar>
-    //   </>
-   
-   <>
-   <header>
-    <img
-      id="profilepic"
-      src="./Assets/images/profile pic.png"
-      alt="profilePicture"
-      width="10%"
-      height="10%"
-    />
-    <h1>Roopa Thimmanacherla</h1>
-    <nav>
-      <a href="#AboutMe" alt="informantion about employee"
-        ><h3><b>AboutMe</b></h3></a
-      >
-      <a href="#Work" alt="employee projects"
-        ><h3><b>Work</b></h3></a
-      >
-      <a href="#contactinfo" alt="employee contact information"
-        ><h3><b>ContactMe</b></h3></a
-      >
-      <a
-        href="./Assets/Roopa_salesforce.pdf"
-        alt="employee resume"
-        target="_blank"
-        ><h3>Resume</h3></a
-      >
-    </nav>
-  </header>
-  </>
-
-    );
-
-
+        <h1>Zeo University Hub</h1>
+        <nav>
+          <Link to="/">
+            <h3>
+              <b>Home</b>
+            </h3>
+          </Link>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/saved">Saved Colleges</Link>
+              <Link onClick={Auth.logout}>Logout</Link>
+            </>
+          ) : (
+            <>
+              <Link to="/login">
+                <h3>
+                  <b>Login</b>
+                </h3>
+              </Link>
+              <Link to="/signup">
+                <h3>
+                  <b>Signup</b>
+                </h3>
+              </Link>
+            </>
+          )}
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default AppNavbar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,28 +1,39 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import SearchColleges from './pages/searchColleges.jsx'
-import SavedColleges from './pages/SavedColleges.jsx'
-
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SearchColleges from "./pages/searchColleges.jsx";
+import SavedColleges from "./pages/SavedColleges.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <h1>Wrong page!</h1>,
     children: [
       {
         index: true,
-        element: <SearchColleges />
-      }, {
-        path: '/saved',
-        element: <SavedColleges />
-      }
-    ]
-  }
-])
+        element: <SearchColleges />,
+      },
+      {
+        path: "/saved",
+        element: <SavedColleges />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
-)
+);
