8318fdf255455a9502bbf323d7c89194ac49f7ac
Jus Ferrell <j1ferrel@ucsd.edu>
2024-03-17 15:48:28 -0700

    fetch college API, general setup

--- client/src/pages/SearchColleges.jsx
+++ client/src/pages/SearchColleges.jsx
@@ -1,11 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from 'axios'; //use axios for scaling
 
 const SearchColleges = () => {
-    const []
+	const [inputValue, setInputValue] = useState('');
+	const [searchSuggestions, setSearchSuggestions] = useState([]);
+	const [searchedColleges, setSearchedColleges] = useState([]);
+	const [searchHistory, setSearchHistory] = useState([]);
+	const [savedColleges, setSavedColleges] = useState([]);
+
+	useEffect(() => {
+		return () => saveColleges(savedColleges);
+	});
+
+	// Function to fetch colleges based on user input
+	const fetchColleges = async (query) => {
+		if (!query) return;
+
+		try {
+			const response = await axios.get(`http://universities-and-colleges.p.rapidapi.com/`, {
+                params: { name: query },
+                headers: {
+                    'X-RapidAPI-Host': 'universities-and-colleges.p.rapidapi.com',
+                    'X-RapidAPI-Key': '447a932fd1msh0d9dd0f0212fec5p10a804jsna36243841f48'
+                }
+            });
+
+			// Assuming the API returns an array of college names
+			setSearchSuggestions(response.data.map((college) => college.name));
+		} catch (error) {
+			console.error('Error fetching college data:', error);
+			setSearchSuggestions([]);
+		}
+	};
 };
-
-
-
 
 export default SearchColleges;
