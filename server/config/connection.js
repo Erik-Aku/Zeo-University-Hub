ddf91b2172192f11fb5e7b34a5e0bdef3cf33f02
Roopa Thimmanacherla <roopareddy90@gmail.com>
2025-04-10 18:37:05 -0700

    edited config file to import mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,5 @@
 const { connect, connection } = require("mongoose");
+const mongoose = require("mongoose");
 
 //connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/zeouniversityhub');
 mongoose.connect(
