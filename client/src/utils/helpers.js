306f2189875c099a297437e601935140ceb2f1ae
Roopa Thimmanacherla <roopareddy90@gmail.com>
2025-04-10 19:09:10 -0700

    efited helper file

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,6 +1,6 @@
-//import CryptoJS from 'crypto-js';
+import CryptoJS from "crypto-js";
 
-const CryptoJS = require("crypto-js");
+//const CryptoJS = require("crypto-js");
 
 export const generateHashId = (name, city, state) => {
   return CryptoJS.SHA256(`${name}-${city}-${state}`).toString(CryptoJS.enc.Hex);
