b0614983bacf5e44a1c61cb5b27bf227666e57f3
Roopa Thimmanacherla <roopareddy90@gmail.com>
2025-04-10 22:06:29 -0700

    edited helper.js file

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -6,8 +6,19 @@
 //   return CryptoJS.SHA256(`${name}-${city}-${state}`).toString(CryptoJS.enc.Hex);
 // };
 
-import { createHash } from "crypto";
+// import { createHash } from "crypto";
 
-export const generateHashId = (name, city, state) => {
-  return createHash("sha256").update(`${name}-${city}-${state}`).digest("hex");
-};
+// export const generateHashId = (name, city, state) => {
+//   return createHash("sha256").update(`${name}-${city}-${state}`).digest("hex");
+// };
+
+export async function generateHashId(name, city, state) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(`${name}-${city}-${state}`);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+  return hashHex;
+}
