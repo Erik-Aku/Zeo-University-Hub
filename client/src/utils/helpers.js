7abd07e099e53fd77e983071e003c84f51c4b46e
Roopa Thimmanacherla <roopareddy90@gmail.com>
2025-04-10 21:44:43 -0700

    edited helper file

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,7 +1,13 @@
-import CryptoJS from "crypto-js";
+// import CryptoJS from "crypto-js";
 
-//const CryptoJS = require("crypto-js");
+// //const CryptoJS = require("crypto-js");
+
+// export const generateHashId = (name, city, state) => {
+//   return CryptoJS.SHA256(`${name}-${city}-${state}`).toString(CryptoJS.enc.Hex);
+// };
+
+import { createHash } from "crypto";
 
 export const generateHashId = (name, city, state) => {
-  return CryptoJS.SHA256(`${name}-${city}-${state}`).toString(CryptoJS.enc.Hex);
+  return createHash("sha256").update(`${name}-${city}-${state}`).digest("hex");
 };
