e54bcadfc5059899f8b50baa34049b4d8c9e7959
Roopa Thimmanacherla <roopareddy90@gmail.com>
2025-04-10 18:20:49 -0700

    added mongodb atlas string URI in connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
-const { connect, connection } = require('mongoose');
+const { connect, connection } = require("mongoose");
 
-connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/zeouniversityhub');
-
+//connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/zeouniversityhub');
+mongoose.connect(
+  "mongodb+srv://roopareddy:password1234@cluster0.gaxh4xl.mongodb.net/College?retryWrites=true&w=majority&appName=Cluster0"
+);
 module.exports = connection;
